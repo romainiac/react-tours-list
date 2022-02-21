215a448d225ad32f4f275836c4532bbf17df559a
unknown <romanyefimets@gmail.com>
2022-02-21 15:38:36 -0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import './App.css';
 
-function App() {
-  return <div className="App"></div>;
-}
-
+const App = () => {
+  return (
+    <div>
+      <h1>Tours</h1>
+    </div>
+  );
+};
 export default App;
