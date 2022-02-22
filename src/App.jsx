11c77e81b67d4caf1bff0678dfcaf142babd911b
unknown <romanyefimets@gmail.com>
2022-02-21 16:03:12 -0800

    tours are listed in tours list with button to remove

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const App = () => {
     </main>
   ) : (
     <main>
-      <TourList />
+      <TourList tours={tours} />
     </main>
   );
 };
--- src/tour-list.component.jsx
+++ src/tour-list.component.jsx
@@ -1,5 +1,19 @@
-const TourList = () => {
-  return <h2>List of Tours</h2>;
+import Tour from './tour.component';
+
+const TourList = ({ tours }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>our Tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default TourList;
